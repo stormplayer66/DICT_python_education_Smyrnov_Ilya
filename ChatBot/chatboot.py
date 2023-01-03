4a87ba1733d59fab87aecd002b571bcce1beabc6
Ilya Smyrnov <i.d.smyrnov@student.khai.edu>
2023-01-03 21:51:27 +0200

     This is a combination of 2 commits.  This is the 1st commit message:
    Stage 5
    
    Stage 4
    
    Stage 3
    
    Stage 2
    
    Stage 1
    
     The commit message #2 will be skipped:
    
     Stage 4

--- ChatBot/chatboot.py
+++ ChatBot/chatboot.py
@@ -11,7 +11,7 @@ remainder7 = int(input("> "))
 age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 
 print("Your age is {0}; that's a good time to start programming!".format(age))
-print("Now I will prove to you that I can count to any number you want.")
+print("Now I will prove to you that I can count to any number you want." )
 number = int(input("> "))
 i = 0
 while i != number+1:
